3be535d704e6292e67613ddb943360a1bb44ad9d
Tetsuro Nakamura <t2low.nk@gmail.com>
2017-07-11 09:40:24 +0900

    フルスクリーン設定

--- app/src/main/java/com/tappli/openglessample/MainActivity.kt
+++ app/src/main/java/com/tappli/openglessample/MainActivity.kt
@@ -3,6 +3,9 @@ package com.tappli.openglessample
 import android.opengl.GLSurfaceView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Fullscreen mode
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE)
 
         glView = MyGLSurfaceView(this)
         setContentView(glView)
